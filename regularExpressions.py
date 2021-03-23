b5f03ed1a9628bf799a85bf95cb66aadf5450afe
Lukas Monico <lukasmonico@gmail.com>
2021-03-23 09:21:24 -0500

    Completed regx assignment, run time n^2 not optimal

--- regularExpressions.py
+++ regularExpressions.py
@@ -2,4 +2,17 @@
 # 3/18/2021
 # Coursera: Using Python to Access Web Data
 
-import re
+import re #regx library
+
+#open text file
+#create for loop to iterate through each line
+#have running sum to "capture" each number
+#print results once end of file reached
+sum = 0
+with open("regex_sum_1041747.txt") as file:
+    for line in file:
+        matches = re.findall('([0-9]+)', line)
+        if(matches != []):
+            for num in matches:
+                sum = sum + int(num)
+print(sum)
