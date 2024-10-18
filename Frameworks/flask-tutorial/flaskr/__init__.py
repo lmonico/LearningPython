8f4ea26971cef6e5c8b137c449abbdd8f8211684
Lukas Monico <Lukas Monico>
2024-10-18 17:11:38 -0600

    feat: started flask tutorial

---
+++ Frameworks/flask-tutorial/flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and config the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load instance config if present and not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load test config if passed in
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return "Hello, world!"
+    
+    from . import db
+    db.init_app(app)
+
+    return app
